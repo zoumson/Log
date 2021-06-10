b90b3e442f478d17e946616cbca46c3a1cf90744
zoumson <stargue49@gmail.com>
2021-06-10 22:39:15 +0800

    change log extension to .log

--- src/Demo.cpp
+++ src/Demo.cpp
@@ -14,15 +14,16 @@ enum sign_severity_level
 */
 int main() 
 {
-    std::cout << "Demo Log manager using boost\n";
+   
     za::my_logger::logger_type log = za::my_logger::get();
     za::Log logManager;
-    logManager.set_log_file("log1.txt");
+    logManager.set_log_file("./log/log1.log");
+    BOOST_LOG_SEV(log, za::report) << "Demo Log manager using boost\n";
     BOOST_LOG_SEV(log, za::report) << "log1--->[1]...\n";
     BOOST_LOG_SEV(log, za::warning) << "log1--->[2]...\n";
 
-    logManager.set_log_file("log2.txt");    
-    BOOST_LOG_SEV(log, za::info) << "log2--->[1]...\n";
+    logManager.set_log_file("./log/log2.log");    
+    BOOST_LOG_SEV(log, za::report)<< "log2--->[1]...\n";
     BOOST_LOG_SEV(log, za::error) << "log2--->[2]...\n";
 
     return 0;
